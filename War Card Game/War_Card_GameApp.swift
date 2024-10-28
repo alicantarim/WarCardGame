96637c74d5adada1758595db86d334c47737c0a7
alicantarim <alicantarim@hotmail.com>
2024-10-29 01:11:23 +0300

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Alican TARIM on 29.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
