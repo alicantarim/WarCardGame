dafcef17ec46414f5f1cadc30b022f4e3d11a625
alicantarim <alicantarim@hotmail.com>
2024-10-29 10:19:55 +0300

    completes the UI of the project.

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -8,15 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var playerCard = "card7"
+    var cpuCard = "card13"
+    
+    var playerScore: Int = 0
+    var cpuScore: Int = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            
+            Image("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Image("logo")
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    Image(playerCard)
+                    
+                    Spacer()
+                    
+                    Image(cpuCard)
+                
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                Button {
+                    deal()
+                } label: {
+                    Image("button")
+                }
+                
+                Spacer()
+                
+                HStack {
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                    }
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                    }
+                    
+                    Spacer()
+                }
+                .foregroundStyle(.white)
+                
+                Spacer()
+            }
         }
-        .padding()
     }
+    
+    func deal() {
+        print("Deal button was tapped.")
+    }
+    
 }
 
 #Preview {
